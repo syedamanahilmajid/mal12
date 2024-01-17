897eb505dc27617c5549886c700c9954a328bad0
syedamanahilmajid <156665624+syedamanahilmajid@users.noreply.github.com>
2024-01-17 16:27:12 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+x=int(input("enter no1"))
+y=int(input("enter no2"))
+c=input("enter an opr (+,-,*,/)")
+if c=="+":
+    print(x + y)
+elif c=="-":
+    print(x - y)
+elif c=="*":
+    print(x * y)
+elif c=="/":
+    print(x / y)
+      
+
+     
